b94be0149121041af9406aa8536fb3900a69f45e
muh-younus <braq369@gmail.com>
2025-09-17 03:06:06 -0700

    add hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 function App() {
   return (
   <>
 
   <Navbar/>
+  <HeroSection/>
   
   </>
   );
---
+++ src/components/HeroSection.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import HeroImage from "../image/profile3.png"
+
+
+function HeroSection() {
+  return (
+    <>
+    <div className="bg-black text-white text-center py-16">
+
+        <img
+        src={HeroImage} alt="profile"
+         className="mx-auto mb-8 w-80 h-80 rounded-full object-cover transform
+          transition-transform duration-300 hover:scale-110"
+        />
+        <h1>
+            I'm {" "}
+            <span>Muhammad Younus</span> 
+            Frontend Developer
+        </h1>
+        <p>
+            I create responsive and engaging web applications.
+        </p>
+        <div>
+            <button>Contact With Me</button>
+            <button>Download CV</button>
+        </div>
+
+
+    </div>
+    </>
+  )
+}
+
+export default HeroSection
